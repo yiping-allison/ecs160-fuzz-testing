75e5c943f590581354cc34fa29cc486fe3881a63
Yiping-Allison <yipings09@gmail.com>
2019-11-23 20:47:47 -0800

    Refactor Code and Cleaning
    * Updated documentation for functions
    * Cleaned comments
    * Refactored implementation in swap()
    * Add new testing files from HW3 -- export ignore in gitattributes

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,8 +1,9 @@
 /**
- * maxTweeter takes in a csv file name and will calculate
- * the top 10 Tweeters by name.
+ * @file maxTweeter.c
+ * @brief Prints top 10 Tweeters from CSV file
  * 
- * Authors: Yiping (Allison) Su and Joanne Chang
+ * @author Yiping (Allison) Su
+ * @author Joanne Chang
  */
 
 #include <stdio.h>
@@ -56,9 +57,9 @@ int findUser(char *name, Link *info);
 void freeLinkedMemory(Node *head);
 int getNameIndex(FILE *fileName);
 void insertAtLast(char *name, Link *info);
-void insertToList(Node *list, char *name, Link *info);
+void insertToList(char *name, Link *info);
 void printList(Node *head, int count);
-void processData(FILE *fileName, int namePos, Node *list, Link *info);
+void processData(FILE *fileName, int namePos, Link *info);
 void swap(Node *left, Node *right, Link *info);
 
 int main(int argc, char *argv[])
@@ -69,7 +70,7 @@ int main(int argc, char *argv[])
 	Node *first = createNode(NULL, 1);
 	info -> head = first;
 	info -> last = first;
-	processData(fileName, namePos, first, info);
+	processData(fileName, namePos, info);
 	printList(info -> head, 10);
 	fclose(fileName);
 	freeLinkedMemory(info -> head);
@@ -78,6 +79,8 @@ int main(int argc, char *argv[])
 }
 
 /**
+ * @brief Extracts index of NAME field from CSV
+ * 
  * parseName takes in a file pointer and returns
  * the index of the name position from the csv file
  *
@@ -90,7 +93,7 @@ int main(int argc, char *argv[])
  * https://www.gnu.org/software/libc/manual/html_node/Finding-Tokens-in-a-String.html
  *  
  * @param fileName The address to where the file is located
- * @return index of the username column
+ * @return Index of the username column
  */
 int getNameIndex(FILE *fileName)
 {
@@ -112,6 +115,8 @@ int getNameIndex(FILE *fileName)
 }
 
 /**
+ * @brief Processes data from given CSV file
+ * 
  * processData takes in a file pointer, index of the Name field, and
  * the pointer to the Empty HEAD of the list and processes the data of the
  * csv file.
@@ -119,9 +124,12 @@ int getNameIndex(FILE *fileName)
  * It will use a while loop to iterate through each line and add tweet info
  * to the linked list using insertList().
  * 
+ * @param fileName Address of file location
+ * @param namePos Index of NAME value in CSV line 
+ * @param info Data struct which contains address of HEAD and TAIL of list
  * @return void
  */
-void processData(FILE *fileName, int namePos, Node *list, Link *info)
+void processData(FILE *fileName, int namePos, Link *info)
 {
 	char buff[MAX_LINE];
 	while (!feof(fileName)) {
@@ -130,15 +138,16 @@ void processData(FILE *fileName, int namePos, Node *list, Link *info)
 			return;
 		}
 		char *name = extractName(str, namePos);
-		insertToList(list, name, info);
+		insertToList(name, info);
 	}
 }
 
 /**
- * extractName takes in a csv line and name position
- * and returns the string at the given index.
- * 
- * @return the supposed 'name' string at the index value
+ * @brief Extracts the name from CSV line given an index
+ *
+ * @param str Address to CSV line
+ * @param namePos Index of NAME value in CSV line 
+ * @return The supposed 'name' string at the index value
  */
 char *extractName(char* str, int namePos)
 {
@@ -156,11 +165,15 @@ char *extractName(char* str, int namePos)
  }
 
 /**
+ * @brief Creates a new node
+ * 
  * createNode takes in a character string and and integer and creates
  * a new Node and new Tweeter value. If the integer equals, 1, it means
  * that we're dealing with the first INITIAL node. Otherwise it's a regular node.
  * 
- * @return The pointer of the new node to be inserted in a list
+ * @param name Address location of NAME to be used
+ * @param initial Integer indicating first node or not
+ * @return The pointer to the new node to be inserted in a list
  *         with filler values
  */
 Node *createNode(char *name, int initial)
@@ -182,15 +195,19 @@ Node *createNode(char *name, int initial)
 }
 
 /**
+ * @brief Handles inserting new nodes into the list
+ * 
  * insertToList takes in a pointer to the start of the doubly
  * linked list and a char string, name.
  * 
  * It inserts the node into the correct part of the list and
  * calls on createNode for new nodes that need to be created.
  *
+ * @param name Address location of NAME to be used
+ * @param info Data struct which contains address of HEAD and TAIL of list
  * @return void
  */
-void insertToList(Node *list, char *name, Link *info)
+void insertToList(char *name, Link *info)
 {
 	if (!(info -> head -> user.name)) {
 		// The node list is empty -- we start at item one
@@ -207,6 +224,8 @@ void insertToList(Node *list, char *name, Link *info)
 }
 
 /**
+ * @brief Finds a user in the doubly-linked list
+ * 
  * findUser takes in a name string and the data struct to the head and
  * tail of the linked list and finds if the username can be found
  * in our current dataset. 
@@ -214,6 +233,8 @@ void insertToList(Node *list, char *name, Link *info)
  * If we find a user, we add the count in our dataset and sort it to its correct
  * place and return 1. Otherwise, we return -1 -- not found.
  * 
+ * @param name Address location of NAME to be used
+ * @param info Data struct which contains address of HEAD and TAIL of list
  * @return 1 or -1 which represents found and not found
  */
 int findUser(char *name, Link *info)
@@ -226,7 +247,7 @@ int findUser(char *name, Link *info)
 			current = current -> next;
 		}
 	}	
-	// if we reach here, we found the user
+	// If we reach here, we found the user
 	++(current -> user.count);
 	if ((current -> prev) == NULL) {
 		// Nothing on the left -- current should be the new HEAD
@@ -236,8 +257,7 @@ int findUser(char *name, Link *info)
 	while ((current -> user.count) > (current -> prev -> user.count)) {
 		swap(current -> prev, current, info);
 		if ((current -> prev) == NULL) {
-			// we reached the end
-			info -> head = current;
+			// We reached the end
 			break;
 		}
 	}
@@ -245,8 +265,11 @@ int findUser(char *name, Link *info)
 }
 
 /**
- * swap will swap two nodes in the doubly-linked list
+ * @brief Swap two nodes in the doubly-linked list
  * 
+ * @param left Address of left node to be swapped
+ * @param right Address of right node to be swapped
+ * @param info Data struct which contains address of HEAD and TAIL of list
  * @return void
  */
 void swap(Node *left, Node *right, Link *info)
@@ -259,9 +282,7 @@ void swap(Node *left, Node *right, Link *info)
 		left -> prev = right;
 		info -> head = right;
 		info -> last = left;
-		return;
-	}
-	if ((left -> prev == NULL) && (right -> next != NULL)) {
+	} else if ((left -> prev == NULL) && (right -> next != NULL)) {
 		// We do a first swap
 		right -> next -> prev = left;
 		left -> prev = right;
@@ -269,9 +290,7 @@ void swap(Node *left, Node *right, Link *info)
 		right -> next = left;
 		right -> prev = NULL;
 		info -> head = right;
-		return;
-	}
-	if ((right -> next == NULL) && (left -> prev != NULL)) {
+	} else if ((right -> next == NULL) && (left -> prev != NULL)) {
 		// We do a last swap
 		left -> prev -> next = right;
 		right -> prev = left -> prev;
@@ -279,25 +298,26 @@ void swap(Node *left, Node *right, Link *info)
 		left -> prev = right;
 		left -> next = NULL;
 		info -> last = left;
-		return;
+	} else {
+		// Regular swap
+		right -> next -> prev = left;
+		left -> prev -> next = right;
+		right -> prev = left -> prev;
+		left -> next = right -> next;
+		right -> next = left;
+		left -> prev = right;
 	}
-	// Regular swap
-	right -> next -> prev = left;
-	left -> prev -> next = right;
-	right -> prev = left -> prev;
-	left -> next = right -> next;
-	right -> next = left;
-	left -> prev = right;
-	return;
 }
 
 /**
+ * @brief Inserts node at end of list
+ * 
  * insertAtLast takes in a name string and the data struct
  * which stores the pointers to the head and tail of the linked list
  * and adds an element to the very end of the linked list.
  * 
- * @param name
- * @param info
+ * @param name Address location of NAME to be used
+ * @param info Data struct which contains address of HEAD and TAIL of list
  * @return void
  */
 void insertAtLast(char *name, Link *info)
@@ -310,11 +330,14 @@ void insertAtLast(char *name, Link *info)
 }
 
 /**
+ * @brief Allocate memory space for name
+ * 
  * allocateName is a utility function which takes in an address
  * pointing to a name char array and copies that name into a new memory
  * location (used for new username creation)
  * 
- * @return the new address to the name
+ * @param nameToCopy The address of string to be copied
+ * @return The new address to the name
  */
 char *allocateName(char *nameToCopy)
 {
@@ -324,17 +347,18 @@ char *allocateName(char *nameToCopy)
 }
 
 /**
- * printList takes in the pointer to the head
- * and prints the linked list.
+ * @brief Prints nodes up to specified integer
  * 
+ * printList takes in the pointer to the head and prints the linked list.
  * It will print the whole linked list when count == -1, else num count.
  * 
+ * @param head The memory location to first node of list
+ * @param count Either the num nodes you want printed or -1 for all
  * @return void 
  */
 void printList(Node *head, int count)
 {
 	if (count == -1) {
-		// print all
 		printf("\nPrinting all elements:\n");
 		while (head != NULL) {
 			printf("%s: %d\n", head -> user.name, head -> user.count);
@@ -353,8 +377,9 @@ void printList(Node *head, int count)
 }
 
 /**
- * freeLinkedMemory frees all the allocated memory in the Node List
+ * @brief Frees all the allocated memory in the Node List
  * 
+ * @param head The memory location to first node of list
  * @return void
  */
 void freeLinkedMemory(Node *head)
